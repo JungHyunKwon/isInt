ac757548b84e7f2c7cb8f9bccf9708a841918551
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-11-12 10:03:39 +0900

    published

---
+++ js/isNumber.js
@@ -0,0 +1,21 @@
+/**
+ * @author JungHyunKwon
+ * @version 1.0.0
+ */
+try {
+	(function() {
+		'use strict';
+
+		/**
+		 * @name isNumber
+		 * @since 2017-12-06
+		 * @param {*} value
+		 * @return {boolean}
+		 */
+		window.isNumber = function(value) {
+			return typeof value === 'number' && !isNaN(value) && isFinite(value);
+		};
+	})();
+}catch(e) {
+	console.error(e);
+}
