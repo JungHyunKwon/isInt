33379002f5b47cc01d00f2bf90bc10b5ac37b536
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-11-18 22:51:23 +0900

    published

--- js/isInt.js
+++ js/isInt.js
@@ -7,12 +7,12 @@ try {
 		'use strict';
 
 		/**
-		 * @name isInteger
+		 * @name isInt
 		 * @since 2017-12-06
 		 * @param {*} value
 		 * @return {boolean}
 		 */
-		window.isInteger = function(value) {
+		window.isInt = function(value) {
 			return typeof value === 'number' && !isNaN(value) && isFinite(value);
 		};
 	})(window.isNaN, window.isFinite);
