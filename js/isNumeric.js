c419dfb44d164086a08e51d429cc5798ce1f2c0c
JungHyunKwon <kjh_3859@naver.com>
2019-07-21 02:11:41 +0900

    published

--- js/isNumeric.js
+++ js/isNumeric.js
@@ -2,20 +2,16 @@
  * @author JungHyunKwon
  * @version 1.0.0
  */
-try {
-	(function() {
-		'use strict';
+(function() {
+	'use strict';
 
-		/**
-		 * @name isNumeric
-		 * @since 2017-12-06
-		 * @param {*} value
-		 * @return {boolean}
-		 */
-		window.isNumeric = function(value) {
-			return typeof value === 'number' && !isNaN(value) && isFinite(value);
-		};
-	})();
-}catch(e) {
-	console.error(e);
-}
+	/**
+	 * @name isNumeric
+	 * @since 2017-12-06
+	 * @param {*} value
+	 * @return {boolean}
+	 */
+	window.isNumeric = function(value) {
+		return typeof value === 'number' && !isNaN(value) && isFinite(value);
+	};
+})();
